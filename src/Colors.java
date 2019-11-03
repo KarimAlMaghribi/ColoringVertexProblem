ef62fe0b1f0aa36608c7fb022de77cb8dee24e61
Karim Rakia <karim.rakia@bitctrl.de>
2019-11-03 14:35:27 +0100

    Implementing Colors, Graphs

---
+++ src/Colors.java
@@ -0,0 +1,21 @@
+import java.lang.reflect.Array;
+
+public class Colors {
+
+    public Colors ( int size_k_vertex) {
+        int[] color = this.fill_constructor(size_k_vertex);
+    }
+
+    // Für das Vertex Coloring eines Graphen wird ein Set aus k-Farben, gleich der Anzahl der Knoten vorbereitet.
+    private int[] fill_constructor (int size_k_vertex) {
+        int [] color = new int[size_k_vertex];
+        for (int i = 0; i< size_k_vertex; i++ ){
+
+            color[i] = i+1;
+            i++;
+        }
+
+        return color;
+
+    }
+}
---
+++ src/Graph.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Graph {
+    // Ein Graph ist eine Struktur eines finiten Sets V ( für Vertex, also die Ecken, bzw. Knoten)
+    // und eines finiten Sets E ( die Edges also die Kanten ).
+
+    int v;
+    int e;
+    Map<Integer, ArrayList<Integer>> map = new HashMap<>();
+
+    public Graph(int v, int e, Map<Integer, ArrayList<Integer>> map) {
+        this.v = v;
+        this.e = e;
+        this.map = map;
+    }
+
+    public int getV() {
+        return v;
+    }
+
+    public void setV(int v) {
+        this.v = v;
+    }
+
+    public int getE() {
+        return e;
+    }
+
+    public void setE(int e) {
+        this.e = e;
+    }
+
+    public Map<Integer, ArrayList<Integer>> getMap() {
+        return map;
+    }
+
+    public void addToGraph(int[] vertex_combination){
+        if (vertex_combination.length == 2){
+
+            if ( map.containsKey(vertex_combination[0])){
+
+                ArrayList<Integer> value = map.get(vertex_combination[0]);
+                value.add(vertex_combination[1]);
+                map.replace(vertex_combination[0], value);
+
+            } else {
+
+                ArrayList<Integer> value = new ArrayList<>();
+                value.add(vertex_combination[1]);
+                map.put(vertex_combination[0], value);
+
+            }
+
+        }
+    }
+
+    public void setMap(Map<Integer, ArrayList<Integer>> map) {
+        this.map = map;
+    }
+}
